dbfcf2c61bd6be65a5c4ab4389a0e76aa3b8a808
logosrema <pneumalogoss@gmail.com>
2025-02-10 13:21:14 +0000

    new push 121

--- app/assets/js/userlist.js
+++ app/assets/js/userlist.js
@@ -573,7 +573,7 @@ $(function () {
     async function addAgent(datas) {
         try {
             ///api/v1/limvo/selfregister
-            const response = await fetch("localhost/chairman_test/api/v1/limvo/register_super_user", {
+            const response = await fetch("chairman_test/api/v1/limvo/register_super_user", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
