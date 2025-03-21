62af15ba123ec4a4d0288143d34bf06167c4dc3a
logosrema <pneumalogoss@gmail.com>
2025-03-21 13:51:09 +0000

    deposit

--- app/assets/js/deposit.js
+++ app/assets/js/deposit.js
@@ -34,7 +34,7 @@ $(function () {
        
       data.forEach((item) => {
         let username = item.reg_type === "email" ? item.email : (item.reg_type === "username" ? item.username : item.contact);
-      
+          let formatedates =  item.date_created.replace(" ", " / ")
           html += `
                       <tr>
                           <td>${item.payment_reference}</td>
@@ -45,7 +45,7 @@ $(function () {
                           <td>${formatMoney(item.amount_paid)}</td>
                           <td>${formatMoney(item.charges)}</td>
                           <td>${formatMoney(item.amount_recieved)}</td>
-                          <td>${item.date_created + ' / ' + item.time_created}</td>
+                          <td>${formatedates}</td>
                           <td>${item.provider ? item.provider: "N/A"}</td>
                           <td>${item.user_mobile}</td>
                           <td>${item.status.charAt(0).toUpperCase() + item.status.slice(1)}</td>
